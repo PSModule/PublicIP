9b10de91ae3144c393a0c0ffdd199207df84bf10
Marius Storhaug <marstor@hotmail.com>
2024-03-14 19:03:41 +0100

    🩹 [Patch]: Add tests for PublicIP (#6)
    ## Description
    
    - Add tests for PublicIP
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

---
+++ tests/PublicIP.Tests.ps1
@@ -0,0 +1,11 @@
+﻿Describe 'PublicIP' {
+    Context 'Module' {
+        It 'The module should be available' {
+            Get-Module -Name 'PublicIP' -ListAvailable | Should -Not -BeNullOrEmpty
+            Write-Verbose (Get-Module -Name 'PublicIP' -ListAvailable | Out-String) -Verbose
+        }
+        It 'The module should be imported' {
+            { Import-Module -Name 'PublicIP' } | Should -Not -Throw
+        }
+    }
+}
