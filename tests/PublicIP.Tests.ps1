337e9c648e4e0ecab39b04db80c5a1c0c6ae6701
Marius Storhaug <marstor@hotmail.com>
2024-03-29 00:52:14 +0100

    🩹 [Patch]: Align code and process (#7)
    ## Description
    
    🩹 [Patch]: Align code and process
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/PublicIP.Tests.ps1
+++ tests/PublicIP.Tests.ps1
@@ -1,11 +1,34 @@
-﻿Describe 'PublicIP' {
+﻿[CmdletBinding()]
+Param(
+    # Path to the module to test.
+    [Parameter()]
+    [string] $Path
+)
+
+Write-Verbose "Path to the module: [$Path]" -Verbose
+
+Describe 'PublicIP' {
     Context 'Module' {
         It 'The module should be available' {
             Get-Module -Name 'PublicIP' -ListAvailable | Should -Not -BeNullOrEmpty
             Write-Verbose (Get-Module -Name 'PublicIP' -ListAvailable | Out-String) -Verbose
         }
         It 'The module should be imported' {
-            { Import-Module -Name 'PublicIP' } | Should -Not -Throw
+            { Import-Module -Name 'PublicIP' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
+        }
+    }
+
+    Context 'Get-PublicIP' {
+        It 'Should return the public IP address' {
+            $PublicIP = Get-PublicIP
+            $PublicIP | Should -Not -BeNullOrEmpty
+            Write-Verbose ($PublicIP | Out-String) -Verbose
+        }
+
+        It 'Should return the public IP address from MyIP' {
+            $PublicIP = Get-PublicIP -Provider MyIP
+            $PublicIP | Should -Not -BeNullOrEmpty
+            Write-Verbose ($PublicIP | Out-String) -Verbose
         }
     }
 }
