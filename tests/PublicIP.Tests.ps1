378cd009356b909823873c3b6ade228513b0b95e
Marius Storhaug <marstor@hotmail.com>
2024-03-28 22:12:06 +0100

    align

--- tests/PublicIP.Tests.ps1
+++ tests/PublicIP.Tests.ps1
@@ -1,11 +1,18 @@
-﻿Describe 'PublicIP' {
+﻿[CmdletBinding()]
+Param(
+    # Path to the module to test.
+    [Parameter()]
+    [string] $Path
+)
+
+Describe 'PublicIP' {
     Context 'Module' {
         It 'The module should be available' {
             Get-Module -Name 'PublicIP' -ListAvailable | Should -Not -BeNullOrEmpty
             Write-Verbose (Get-Module -Name 'PublicIP' -ListAvailable | Out-String) -Verbose
         }
         It 'The module should be imported' {
-            { Import-Module -Name 'PublicIP' } | Should -Not -Throw
+            { Import-Module -Name 'PublicIP' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
         }
     }
 }
