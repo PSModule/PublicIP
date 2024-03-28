ad0275000e5f7a4c8208e98351e4cf2fc1ec6e05
Marius Storhaug <marstor@hotmail.com>
2024-03-29 00:46:41 +0100

    fix

--- tests/PublicIP.Tests.ps1
+++ tests/PublicIP.Tests.ps1
@@ -22,13 +22,13 @@ Describe 'PublicIP' {
         It 'Should return the public IP address' {
             $PublicIP = Get-PublicIP
             $PublicIP | Should -Not -BeNullOrEmpty
-            Write-Verbose $PublicIP -Verbose
+            Write-Verbose ($PublicIP | Out-String) -Verbose
         }
 
         It 'Should return the public IP address from MyIP' {
             $PublicIP = Get-PublicIP -Provider MyIP
             $PublicIP | Should -Not -BeNullOrEmpty
-            Write-Verbose $PublicIP -Verbose
+            Write-Verbose ($PublicIP | Out-String) -Verbose
         }
     }
 }
