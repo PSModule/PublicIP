ec4501fb2900842e6995014ca91345114637144f
Marius Storhaug <marstor@hotmail.com>
2024-04-14 01:18:15 +0200

    🩹 [Patch]: Remove module import test (#10)
    ## Description
    
    - Remove module import test
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/PublicIP.Tests.ps1
+++ tests/PublicIP.Tests.ps1
@@ -8,16 +8,6 @@ Param(
 Write-Verbose "Path to the module: [$Path]" -Verbose
 
 Describe 'PublicIP' {
-    Context 'Module' {
-        It 'The module should be available' {
-            Get-Module -Name 'PublicIP' -ListAvailable | Should -Not -BeNullOrEmpty
-            Write-Verbose (Get-Module -Name 'PublicIP' -ListAvailable | Out-String) -Verbose
-        }
-        It 'The module should be imported' {
-            { Import-Module -Name 'PublicIP' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
-        }
-    }
-
     Context 'Get-PublicIP' {
         It 'Should return the public IP address' {
             $PublicIP = Get-PublicIP
